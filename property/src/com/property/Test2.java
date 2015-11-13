580820738bc815a436a286729a086ced8064b3bd
linzh <linzhenhua2010@126.com>
2015-11-13 14:36:26 +0800

    test2

---
+++ property/src/com/property/Test2.java
@@ -0,0 +1,16 @@
+package com.property;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                //sign from github
+		//new 
+		
+		//this line edit from github
+		//this line edit from github second time
+		//this line edit from eclipse first time
+		
+	}
+}
+ 
